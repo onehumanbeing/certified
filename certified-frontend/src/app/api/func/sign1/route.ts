367154e5d4d3e98b4758f09f4b1dd367ec3f01a6
wg326 <57165451+wg326@users.noreply.github.com>
2024-04-12 23:44:24 -0700

    Fix import statement in user route added api route

--- certified-frontend/src/app/api/func/sign1/route.ts
+++ certified-frontend/src/app/api/func/sign1/route.ts
@@ -1,9 +1,8 @@
-
 export const dynamic = "force-dynamic"
 import { UserType } from "@/context/userContext"
 import prisma from "@/lib/prisma/db"
 
-export async function GET(request: Request) {
+export async function POST(request: Request) {
     try {
         const userInfoCookie = request.headers
             .get("cookie")
@@ -23,25 +22,6 @@ export async function GET(request: Request) {
         const userInfo = decodeURIComponent(userInfoCookie)
         try {
             const user: UserType = JSON.parse(userInfo)
-
-            await prisma.user.upsert({
-                where: { email: user.email },
-                create: {
-                    email: user.email,
-                    username: null,
-                    dynamic_id: user.id,
-                },
-                update: {
-                    lastSeemAt: new Date(),
-                },
-                select: {
-                    id: true,
-                    email: true,
-                    createdAt: true,
-                    lastSeemAt: true,
-                    dynamic_id: true,
-                },
-            })
 
             return new Response(JSON.stringify({ user }), {
                 status: 200,
