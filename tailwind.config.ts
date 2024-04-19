96c664c39f276c5f7a9393467da2bacc33fb860c
wg326 <57165451+wg326@users.noreply.github.com>
2024-04-19 16:58:00 -0700

    Update dark theme in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
     plugins: [require("daisyui")],
     daisyui: {
         themes: ["sunset", "cupcake", "cyberpunk"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-        darkTheme: "sunset", // name of one of the included themes for dark mode
+        darkTheme: "cupcake", // name of one of the included themes for dark mode
         base: true, // applies background color and foreground color for root element by default
         styled: true, // include daisyUI colors and design decisions for all components
         utils: true, // adds responsive and modifier utility classes
