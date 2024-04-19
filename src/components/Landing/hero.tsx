90c56104ecbcb15cec94fd7593a22f29c475c273
wg326 <57165451+wg326@users.noreply.github.com>
2024-04-19 02:09:07 -0700

    Add logo to hero section

--- src/components/Landing/hero.tsx
+++ src/components/Landing/hero.tsx
@@ -1,11 +1,15 @@
 "use client"
+import Image from "next/image"
 import { useRouter } from "next/navigation"
 
 export default function Hero() {
     const router = useRouter()
     return (
-        <div className="hero-content text-center">
-            <div className="max-w-md">
+        <div className="hero-content text-center flex w-full justify-between">
+            <div className="w-1/2">
+                <Image src="/assets/certified_logo.png" width={130} height={39} alt="Logo" />
+            </div>
+            <div className="w-1/2">
                 <h1 className="text-5xl font-bold "></h1>
                 <p className="py-6">Making Certification Simple</p>
                 <button
