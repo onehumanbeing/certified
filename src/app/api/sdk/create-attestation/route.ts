2c40811b1a059530cb49528f727803cd28db76b8
Henry Yuan <henry.yuan@nyu.edu>
2024-06-06 13:29:49 -0400

    fix

--- src/app/api/sdk/create-attestation/route.ts
+++ src/app/api/sdk/create-attestation/route.ts
@@ -15,26 +15,26 @@ export async function POST(request: Request) {
             attester,
             holder_name,
             metadata,
-            data,
+            data
         } = object
         try {
+            console.log("Request data:", object)
             const attestationInfo = await createAttestationFromMessage(data)
             // Create a new attestation record using the fetched schema and template
-            const newAttestationRecord = await prisma.attestationRecord.create({
-                data: {
-                    name: holder_name,
-                    email: "",
-                    walletAddress: attester,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                    expirationAt: "",
-                    schemaId: schemaId as string,
-                    attestationId: attestationInfo.attestationId,
-                    schema: schemaId, // store the schema ID here
-                    template: metadata
-                },
-            })
-
+            // const newAttestationRecord = await prisma.attestationRecord.create({
+            //     data: {
+            //         name: holder_name,
+            //         email: "",
+            //         walletAddress: attester,
+            //         createdAt: new Date(),
+            //         updatedAt: new Date(),
+            //         expirationAt: "",
+            //         schemaId: schemaId as string,
+            //         attestationId: attestationInfo.attestationId,
+            //         schema: schemaId, // store the schema ID here
+            //         template: metadata
+            //     },
+            // })
             // return the result (attestationId and the templateId)
             return new Response(
                 JSON.stringify({
