8a8584628d77e21d2b5325f3b28e61cf0b38abaf
wg326 <57165451+wg326@users.noreply.github.com>
2024-06-18 19:19:39 -0700

    Refactor COA page component and add DisplayPdf component

--- src/app/coa/[attestation]/[edition]/page.tsx
+++ src/app/coa/[attestation]/[edition]/page.tsx
@@ -1,14 +1,12 @@
 "use server"
 
-import {
-    Certificate
-} from "certified-sdk";
 import prisma from "@/lib/prisma/db"
+import DisPlayPdf from "@/components/coa/displayPdf"
 
 export type COARecord = {
     attestationId: string
     attester: string
-    data: string
+    data: any
 }
 
 async function getItem(attestationId: string): Promise<COARecord | null> {
@@ -21,61 +19,20 @@ async function getItem(attestationId: string): Promise<COARecord | null> {
         return null
     }
     const coa: COARecord = {
-        ...res
+        ...res,
     }
     return coa
 }
 
-export default async function Page({ params }: { params: { attestation: string, edition: number } }) {
-    let coa: COARecord | null = null;
-    let data: any;
-    let metadata: any;
-    let extra: any;
-
-    coa = await getItem(params.attestation);
-    if(coa !== null) {
-        data = JSON.parse(coa?.data!);
-        metadata = JSON.parse(data.metadata);
-        extra = JSON.parse(data.extra);
-    }
+export default async function Page({
+    params,
+}: {
+    params: { attestation: string; edition: number }
+}) {
+    const coa = await getItem(params.attestation)
     return (
-        <>
-            {coa !== null ? (
-                <Certificate 
-                    artworkTitle={metadata.artworkTitle}
-                    artistName={metadata.artistName}
-                    yearOfCompletion={metadata.yearOfCompletion}
-                    dimensions={metadata.dimensions}
-                    editionNumber={`${params.edition}/${extra.editionNumber}`}
-                    medium={metadata.medium}
-                    registrationNumber={metadata.registrationNumber}
-                    dateOfCertification="21/12/2025"
-                    signatureImagePath={extra.signatureImageUrl}
-                    artworkImagePath={extra.imageUrl}
-                    markerImagePath={extra.markerImageUrl}
-                    certificateUrl={`https://scan.sign.global/attestation/${coa.attestationId}`}
-                />
-            ) : (
-                <div className="hero min-h-screen bg-base-200 text-[12px] w-full overflow-auto px-[50px] flex justify-center gap-4 text-xl font-bold">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="lucide lucide-circle-x"
-                    >
-                        <circle cx="12" cy="12" r="10" />
-                        <path d="m15 9-6 6" />
-                        <path d="m9 9 6 6" />
-                    </svg>
-                    COA not exist...
-                </div>
-            )}
-        </>
+        <div className="w-full h-full fixed z-[1000] bg-white">
+            <DisPlayPdf params={params} coa={coa} />
+        </div>
     )
 }
---
+++ src/components/coa/displayPdf.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { COARecord } from "@/app/coa/[attestation]/[edition]/page"
+import { Certificate } from "certified-sdk"
+import { FC, useEffect, useState } from "react"
+
+interface DisPlayPdfProps {
+    params: { attestation: string; edition: number }
+    coa: COARecord | null
+}
+
+const DisPlayPdf: FC<DisPlayPdfProps> = ({ params, coa }) => {
+    const [coaRecord, setCoaRecord] = useState<COARecord | null>(null)
+    const [metadata, setMetadata] = useState<any>()
+    const [extra, setExtra] = useState<any>()
+
+    useEffect(() => {
+        if (coa !== null) {
+            const parsedData = JSON.parse(coa?.data!)
+            setMetadata(JSON.parse(parsedData.metadata))
+            setExtra(JSON.parse(parsedData.extra))
+            setCoaRecord(coa)
+        }
+    }, [coa])
+
+    return (
+        <>
+            {coaRecord !== null ? (
+                <div className="w-full h-full">
+                    <Certificate
+                        artworkTitle={metadata.artworkTitle}
+                        artistName={metadata.artistName}
+                        yearOfCompletion={metadata.yearOfCompletion}
+                        dimensions={metadata.dimensions}
+                        editionNumber={`${params.edition}/${extra.editionNumber}`}
+                        medium={metadata.medium}
+                        registrationNumber={metadata.registrationNumber}
+                        dateOfCertification="21/12/2025"
+                        signatureImagePath={extra.signatureImageUrl}
+                        artworkImagePath={extra.imageUrl}
+                        markerImagePath={extra.markerImageUrl}
+                        certificateUrl={`https://scan.sign.global/attestation/${coa?.attestationId}`}
+                    />
+                </div>
+            ) : (
+                <div className="hero min-h-screen bg-base-200 text-[12px] w-full overflow-auto px-[50px] flex justify-center gap-4 text-xl font-bold">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="lucide lucide-circle-x"
+                    >
+                        <circle cx="12" cy="12" r="10" />
+                        <path d="m15 9-6 6" />
+                        <path d="m9 9 6 6" />
+                    </svg>
+                    COA not exist...
+                </div>
+            )}
+        </>
+    )
+}
+
+export default DisPlayPdf
