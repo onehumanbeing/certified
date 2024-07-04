d9ab5a68cce451c504e47b7f36ea0981569d3d21
wg326 <57165451+wg326@users.noreply.github.com>
2024-07-04 14:32:59 -0700

    fix

--- src/components/coa/displayPdf.tsx
+++ src/components/coa/displayPdf.tsx
@@ -2,7 +2,7 @@
 
 import { COARecord } from "@/app/coa/[attestation]/[edition]/page"
 // import { Certificate, CertificateTemplate } from "certified-sdk"
-import CertificateTemplate from './CertificateTemplate'
+import CertificateTemplate from "./CertificateTemplate"
 import { FC, useEffect, useRef, useState } from "react"
 import domtoimage from "dom-to-image"
 import jsPDF from "jspdf"
@@ -21,78 +21,105 @@ const DisPlayPdf: FC<DisPlayPdfProps> = ({ params, coa }) => {
     const router = useRouter()
 
     const generatePdf = () => {
-        console.log(certificateRef.current, coaRecord);
         if (certificateRef.current && coaRecord) {
             // Create an array to store all the image load promises
-            const imageLoadPromises: Promise<any>[] = [];
+            const imageLoadPromises: Promise<void>[] = []
+
             // Add a promise for each image in the Certificate component
-            certificateRef.current.querySelectorAll('img').forEach((imgElement) => {
-                console.log(imgElement.src)
-                imageLoadPromises.push(new Promise((resolve, reject) => {
-                    imgElement.onload = resolve;
-                    imgElement.onerror = reject;
-                }));
-         });
-        // Wait for all images to load
-        Promise.all(imageLoadPromises).then(() => {
-            var scale = 2;
-            const w = 864 * scale;
-            const h = certificateRef.current!.offsetHeight * scale;
-            console.log(w, h)
-            domtoimage
-                .toPng(certificateRef.current!, {
-                    width: w,
-                    height: h,
-                    style: {
-                        transform: 'scale('+scale+')',
-                        transformOrigin: 'top left'
+            certificateRef.current.querySelectorAll("img").forEach((imgElement) => {
+                // console.log(imgElement.src)
+                const promise = new Promise<void>((resolve, reject) => {
+                    // Check if the image is already loaded
+                    if (imgElement.complete && imgElement.naturalHeight !== 0) {
+                        resolve(undefined) // Pass `undefined` explicitly
+                    } else {
+                        imgElement.onload = () => resolve(undefined)
+                        imgElement.onerror = () =>
+                            reject(new Error(`Failed to load image ${imgElement.src}`))
                     }
                 })
-                .then((dataUrl) => {
-                    const img = new Image()
-                    img.src = dataUrl
+                imageLoadPromises.push(promise)
+            })
 
-                    img.onload = () => {
-                        const canvas = document.createElement("canvas")
-                        canvas.width = w
-                        canvas.height = h
-                        const ctx = canvas.getContext("2d")
-                        if (ctx) {
-                            // Define the source rectangle
-                            const sourceX = 0
-                            const sourceY = 0
-                            const sourceWidth = img.width
-                            const sourceHeight = img.height
+            // Wait for all images to load
+            Promise.all(imageLoadPromises)
+                .then(() => {
+                    if (certificateRef.current) {
+                        // Check for non-null access
+                        var scale = 2
+                        const w = 864 * scale
+                        const h = certificateRef.current.offsetHeight * scale
+                        // console.log(w, h)
 
-                            // Define the destination rectangle
-                            const destX = 0
-                            const destY = 0
-                            const destWidth = w
-                            const destHeight = h
-
-                            ctx.drawImage(img, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight)
-                            const dataUrl = canvas.toDataURL("image/png")
-
-                            const imgWidth = w
-                            const imgHeight = h
-                            const orientation = imgWidth > imgHeight ? 'landscape' : 'portrait';
-                            const pdf = new jsPDF({
-                                orientation,
-                                unit: "px",
-                                format: [imgWidth, imgHeight],
+                        domtoimage
+                            .toPng(certificateRef.current, {
+                                width: w,
+                                height: h,
+                                style: {
+                                    transform: "scale(" + scale + ")",
+                                    transformOrigin: "top left",
+                                },
                             })
-                            pdf.addImage(dataUrl, "PNG", 0, 0, imgWidth, imgHeight)
-                            const pdfBlob = pdf.output("blob")
-                            const url = URL.createObjectURL(pdfBlob)
-                            pdf.save("certificate.pdf")
-                            window.location.href = url
-                        }
+                            .then((dataUrl) => {
+                                const img = new Image()
+                                img.src = dataUrl
+
+                                img.onload = () => {
+                                    const canvas = document.createElement("canvas")
+                                    canvas.width = w
+                                    canvas.height = h
+                                    const ctx = canvas.getContext("2d")
+                                    if (ctx) {
+                                        // Define the source and destination rectangles
+                                        const sourceX = 0
+                                        const sourceY = 0
+                                        const sourceWidth = img.width
+                                        const sourceHeight = img.height
+                                        const destX = 0
+                                        const destY = 0
+                                        const destWidth = w
+                                        const destHeight = h
+
+                                        ctx.drawImage(
+                                            img,
+                                            sourceX,
+                                            sourceY,
+                                            sourceWidth,
+                                            sourceHeight,
+                                            destX,
+                                            destY,
+                                            destWidth,
+                                            destHeight
+                                        )
+                                        const dataUrl = canvas.toDataURL("image/png")
+
+                                        const imgWidth = w
+                                        const imgHeight = h
+                                        const orientation =
+                                            imgWidth > imgHeight ? "landscape" : "portrait"
+                                        const pdf = new jsPDF({
+                                            orientation,
+                                            unit: "px",
+                                            format: [imgWidth, imgHeight],
+                                        })
+                                        pdf.addImage(dataUrl, "PNG", 0, 0, imgWidth, imgHeight)
+                                        const pdfBlob = pdf.output("blob")
+                                        const url = URL.createObjectURL(pdfBlob)
+                                        pdf.save("certificate.pdf")
+                                        window.location.href = url
+                                    }
+                                }
+                            })
+                            .catch((error) => {
+                                console.error("DOM to image conversion failed:", error)
+                            })
                     }
                 })
                 .catch((error) => {
-                    console.error("DOM to image conversion failed:", error)
+                    console.error("Image loading failed:", error)
                 })
-            })
+        } else {
+            console.error("certificateRef.current is null or coaRecord is missing")
         }
     }
 
@@ -102,8 +129,8 @@ const DisPlayPdf: FC<DisPlayPdfProps> = ({ params, coa }) => {
             setMetadata(JSON.parse(parsedData.metadata))
             const extraData = JSON.parse(parsedData.extra)
             setExtra(extraData)
-            if(params.edition > extraData.editionNumber || params.edition < 1) {
-                console.log("edition number out of range")
+            if (params.edition > extraData.editionNumber || params.edition < 1) {
+                // console.log("edition number out of range")
                 return
             }
             setCoaRecord(coa)
